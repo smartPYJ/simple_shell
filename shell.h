c3df0476ea99fd5f53618207370b47d7ebfa5965
meetkome <kvictory1010@gmail.com>
2022-07-30 05:41:30 -0700

    header files

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#endif
