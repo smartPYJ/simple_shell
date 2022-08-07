78c361c600563d2945577f76494fca21ad576142
meetkome <kvictory1010@gmail.com>
2022-08-06 20:28:26 -0700

    header

--- shell.h
+++ shell.h
@@ -8,7 +8,31 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void disp_cmd(void);
+#define UNUSED(x) (void)(x)
+
+/**
+ * struct input - variables
+ * @tokens: command line arguments
+ * @buffer: buffer of command
+ * @env: environment variables
+ * @count: count of commands entered
+ * @argv: arguments at opening of shell
+ * @status: exit status
+ * @commands: commands to execute
+ */
+typedef struct input
+{
+	char **tokens;
+	char *buffer;
+	char **env;
+	size_t count;
+	char **argv;
+	int status;
+	char **commands;
+} input_t;
+
+
+void read_cmd(void);
 void tokenizer(char *buf, char *buf_cpy);
 void exec_cmd(char **argv);
 
