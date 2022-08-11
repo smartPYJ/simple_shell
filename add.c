2a8deb2534922205cff5174d6b9896df4f9220b7
Smart Monday <uchemonday01@gmail.com>
2022-08-10 17:32:41 -0700

    fixed betty

--- add.c
+++ add.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
+/**
+ * main - main function
+ *Return: 0
+ */
+
 int main(void)
 {
 	int a = 3;
@@ -12,5 +17,5 @@ int main(void)
 
 	printf("The sum id %d\n", sum);
 	printf("ppid is %d\n", ppid);
-	return 0;
+	return (0);
 }
